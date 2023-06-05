76996a847964f7e5aa6cf343bd7678083f305e8c
mehdiakacem <mehdiakacem20@gmail.com>
2023-06-05 10:31:17 +0100

    tests for ex00 doen

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:13:18 by makacem           #+#    #+#             */
-/*   Updated: 2023/05/28 16:25:36 by makacem          ###   ########.fr       */
+/*   Updated: 2023/06/05 10:29:36 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,37 @@ int main()
 {
     try
     {
-        Bureaucrat mehdi("mehdi", 1);
-        Bureaucrat karim("karim", 150);
-        Bureaucrat simo("simo", 600);
-        Bureaucrat reda("reda", -65);
-
+        Bureaucrat mehdi("mehdi", 69);
         std::cout << mehdi;
-        std::cout << karim;
-        mehdi.decrementGrade();
+    }
+    catch (std::exception & e)
+    {
+        std:: cout << e.what();
+    }
+    std::cout << std::endl;
+    try
+    {
+        Bureaucrat youssef("youssef", -54);
+        std::cout << youssef;
+    }
+    catch (std::exception & e)
+    {
+        std:: cout << e.what();
+    }
+    std::cout << std::endl;
+    try
+    {
+        Bureaucrat omar("omar", 263);
+        std::cout << omar;
+    }
+    catch (std::exception & e)
+    {
+        std:: cout << e.what();
+    }
+    std::cout << std::endl;
+    try
+    {
+        Bureaucrat mehdi("mehdi", 2);
         std::cout << mehdi;
         mehdi.incrementGrade();
         std::cout << mehdi;
@@ -34,7 +57,20 @@ int main()
     {
         std:: cout << e.what();
     }
-
-
+    std::cout << std::endl;
+    try
+    {
+        Bureaucrat mehdi("mehdi", 149);
+        std::cout << mehdi;
+        mehdi.decrementGrade();
+        std::cout << mehdi;
+        mehdi.decrementGrade();
+        std::cout << mehdi;
+    }
+    catch (std::exception & e)
+    {
+        std:: cout << e.what();
+    }
+    std::cout << std::endl;
     return 0;
 }
