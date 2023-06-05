78458b29c1c287e2cb99b63e032f0fc11b13ba17
mehdiakacem <mehdiakacem20@gmail.com>
2023-06-05 11:13:08 +0100

    tests for ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: makacem <makacem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:13:18 by makacem           #+#    #+#             */
-/*   Updated: 2023/06/03 18:55:32 by makacem          ###   ########.fr       */
+/*   Updated: 2023/06/05 11:11:39 by makacem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,89 @@
 
 int main()
 {
-    Bureaucrat mehdi("mehdi", 4);
-
-    PresidentialPardonForm object("object");
-
-    mehdi.signForm(object);
-    mehdi.executeForm(object);
     
+    try
+    {
+        Bureaucrat mehdi("mehdi", 4);
+        ShrubberyCreationForm form("garden");
+
+        mehdi.signForm(form);
+        form.execute(mehdi);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+
+    try
+    {
+        Bureaucrat mehdi("mehdi", 4);
+        RobotomyRequestForm form("garden");
+
+        mehdi.signForm(form);
+        form.execute(mehdi);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+
+    try
+    {
+        Bureaucrat mehdi("mehdi", 4);
+        PresidentialPardonForm form("garden");
+
+        mehdi.signForm(form);
+        form.execute(mehdi);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+
+    try
+    {
+        Bureaucrat mehdi("mehdi", 24);
+        PresidentialPardonForm form("garden");
+
+        mehdi.signForm(form);
+        form.execute(mehdi);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+
+    try
+    {
+        Bureaucrat mehdi("mehdi", 4);
+        PresidentialPardonForm form("garden");
+
+        mehdi.signForm(form);
+        mehdi.executeForm(form);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
+
+    try
+    {
+        Bureaucrat mehdi("mehdi", 24);
+        PresidentialPardonForm form("garden");
+
+        mehdi.executeForm(form);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << std::endl;
 
     return 0;
 }
